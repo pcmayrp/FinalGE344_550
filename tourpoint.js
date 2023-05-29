a4fa0c7027d3091645d99764e4e6703779ed3a2b
pcmayrp <124546148+pcmayrp@users.noreply.github.com>
2023-05-29 12:09:17 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,145 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "เจ๊ไฝ",
+          "คะแนนความนิยม": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.50469321304672,
+            13.75257463037289
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "ร้านอาหารเอกเขนก",
+          "คะแนนความนิยม": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.07694000000163,
+            13.686503999999886
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "บาอิค บาอิค",
+          "คะแนนความนิยม": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.58698900000155,
+            13.9273390000008
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "เรือนเพชร สุกี้ยากี้",
+          "คะแนนความนิยม": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.63472372883564,
+            13.6691146998417
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "แสนแซ่บ",
+          "คะแนนความนิยม": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.62508300000201,
+            13.741244421758125
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "รวยเรือนแพ",
+          "คะแนนความนิยม": 7
+        },
+        "geometry": {
+          "coordinates": [
+            101.07323527116506,
+            13.680117000000223
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "เสวย",
+          "คะแนนความนิยม": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.56807654232495,
+            13.720209422694694
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "ศรณ์",
+          "คะแนนความนิยม": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.56853999999748,
+            13.723043999998637
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "บ้านไม้ริมน้ำ",
+          "คะแนนความนิยม": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.08215200000018,
+            13.690328848039556
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "ชื่อ": "ครัวแม่ส้มเกลี้ยง",
+          "คะแนนความนิยม": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.07259254232633,
+            13.669150000000698
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
